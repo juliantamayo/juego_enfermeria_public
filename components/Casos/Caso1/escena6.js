79caf8a89ad90a09ef2411c2be231b4795d293cf
juliantamayo <julian-9419@hotmail.com>
2020-05-09 17:46:03 -0500

    PAE

--- components/Casos/Caso1/escena6.js
+++ components/Casos/Caso1/escena6.js
@@ -3,71 +3,7 @@ import React, { Component } from 'react'
 import { Text, View, Image, Dimensions, ScrollView, StatusBar, ImageBackground } from 'react-native'
 import Swiper from 'react-native-swiper'
 import style from './../../Style.js';
-const { width } = Dimensions.get('window')
-
-const styles = {
-  wrapper: {},
-  slide: {
-    flex: 1,
-   // justifyContent: 'center',
-    backgroundColor: 'transparent'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  image: {
-    width,
-    flex: 1
-  },
-  paginationStyle: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10
-  },
-  paginationText: {
-    color: 'black',
-    fontSize: 20
-  },
-   modalText: {
-   // marginBottom: 15,
-    textAlign: "justify",
-    fontSize : 18,
-     
-  },
-  modalTextcuerpo: {
-   // marginBottom: 15,
-    textAlign: "left",
-     marginLeft : 10,
-  },
-   modalTextnegrillaTitulo: {
-   // marginBottom: 15,
-   fontSize : 23,
-    textAlign: "center",
-    fontWeight: "bold",
-     borderWidth: 4,
-     backgroundColor: 'white'
-  },
-   modalTextnegrilla: {
-   // marginBottom: 15,
-   fontSize : 18,
-    textAlign: "left",
-    fontWeight: "bold",
-     marginLeft : 3,
-  },
-  margen:{
-    margin:5
-  },
-  bordeview:{
-    borderWidth: 2,
-     margin:5,
-    backgroundColor: 'white'
-  },
-  fondoviewText:{
-    backgroundColor: "rgba(0, 185, 188, 0.37)"
-  }
-}
+import styles from '../../Style_escenas6.js';
 
 const renderPagination = (index, total, context) => {
   return (
