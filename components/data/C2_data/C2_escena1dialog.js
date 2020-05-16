1b6b0246c150dcdc0d648f2af8e52cbcd88588c4
Kncardenas <nataliacardenas0987@gmail.com>
2020-05-16 13:27:57 -0500

    introduccion caso 2 y caso 3

--- components/data/C2_data/C2_escena1dialog.js
+++ components/data/C2_data/C2_escena1dialog.js
@@ -1,10 +1,56 @@
 const questions = [
-  {
+   {
     image: require("../../../assets/images/enfermera.png"),
     personaje: "ENFERMERA",
-    Text: "Hola, soy la Enfermera KARLA"
+    Text: "Hola, soy la Enfermera Leidy"
+  },
+  {
+    image: require("../../../assets/images/enfermera1.png"),
+    personaje: "ENFERMERA",
+    Text: "Su misión será valorar a los pacientes que se le asignen "
+   
+  },
+  {
+    image: require("../../../assets/images/enfermera2.png"),
+    personaje: "ENFERMERA",
+    Text: "e identificar el diagnóstico de Enfermería prioritario según las necesidades del paciente"
+  },
+  {
+    image: require("../../../assets/images/enfermera3.png"),
+    personaje: "ENFERMERA",
+    Text: "El Señor P.C.P de 55 años que consulta al servicio de consulta externa en cita por primera vez de adultez"
+   
+  },
+  {
+    image: require("../../../assets/images/enfermera4.png"),
+    personaje: "ENFERMERA",
+    Text: "El paciente dijo que desea ser valorado de pies a cabeza para saber cómo está "
+   
+  },{
+    image: require("../../../assets/images/enfermera4.png"),
+    personaje: "ENFERMERA",
+    Text: "dice que lo motiva cuidarse cada día más y aprender cómo hacerlo. "
+   
+  } ,
+  {
+    image: require("../../../assets/images/enfermera1.png"),
+    personaje: "ENFERMERA",
+    Text: "El Señor P.C.P cuenta con un adecuado estado de salud, mantiene buena relación familiar. "
+   
+  }, 
+   {
+    image: require("../../../assets/images/enfermera3.png"),
+    personaje: "ENFERMERA",
+    Text: "Trabaja como ingeniero. Su situación socioeconómica es buena. Le gusta leer. Es diestro. Persona activa, pensante, camina adecuadamente"
+   
+  }, 
+     {
+    image: require("../../../assets/images/enfermera2.png"),
+    personaje: "ENFERMERA",
+    Text: "dice que se siente sano y tranquilo, vive satisfecho con su vida."
    
   }
+  
   
 ];
 
--- components/data/C3_data/C3_escena1dialog.js
+++ components/data/C3_data/C3_escena1dialog.js
@@ -2,8 +2,63 @@ const questions = [
   {
     image: require("../../../assets/images/enfermera.png"),
     personaje: "ENFERMERA",
-    Text: "Hola, soy la Enfermera KARLA"
+    Text: "Hola, soy la Enfermera Leidy"
+  },
+  {
+    image: require("../../../assets/images/enfermera1.png"),
+    personaje: "ENFERMERA",
+    Text: "Recuerde que su misión será valorar a los pacientes que se le asignen "
    
+  },
+  {
+    image: require("../../../assets/images/enfermera2.png"),
+    personaje: "ENFERMERA",
+    Text: "e identificar el diagnóstico de Enfermería prioritario según las necesidades del paciente"
+  },
+    {
+    image: require("../../../assets/images/enfermera3.png"),
+    personaje: "ENFERMERA",
+    Text: "El señor AT es un vendedor divorciado de 50 años de edad,  "
+  },
+    {
+    image: require("../../../assets/images/enfermera4.png"),
+    personaje: "ENFERMERA",
+    Text: "refiere que se encontraba de viaje trayendo mercancía para sus ventas"
+  },
+    {
+    image: require("../../../assets/images/enfermera4.png"),
+    personaje: "ENFERMERA",
+    Text: "cuando le apareció un dolor abdominal en el según él mismo, la boca del estómago."
+  },
+    {
+    image: require("../../../assets/images/enfermera2.png"),
+    personaje: "ENFERMERA",
+    Text: "Tratando de aliviar un poco su dolor tomó un jugo de aloe vera, pero con poca mejoría y la molestia persistió"
+  },
+      {
+    image: require("../../../assets/images/enfermera.png"),
+    personaje: "ENFERMERA",
+    Text: "aunque por tener tanta ocupación ha tratado de no prestarle atención"
+  },
+      {
+    image: require("../../../assets/images/enfermera1.png"),
+    personaje: "ENFERMERA",
+    Text: "Pero al segundo día el dolor se intensificó, acompañado de nauseas teniendo que consultar al médico."
+  },
+        {
+    image: require("../../../assets/images/enfermera3.png"),
+    personaje: "ENFERMERA",
+    Text: "El Señor A.T  camina algo lento por sensación de dolor abdominal superior"
+  },
+        {
+    image: require("../../../assets/images/enfermera3.png"),
+    personaje: "ENFERMERA",
+    Text: "en ocasiones se dirige su mano a la zona del ombligo y zona superior del abdomen"
+  },
+        {
+    image: require("../../../assets/images/enfermera4.png"),
+    personaje: "ENFERMERA",
+    Text: "presenta náuseas y esta con piel pálida, y fría al tacto."
   }
   
 ];
