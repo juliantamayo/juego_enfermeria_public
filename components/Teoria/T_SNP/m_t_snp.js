d12c9a9615ead94c82a34f002eef2dbde3da4ca9
Kncardenas <nataliacardenas0987@gmail.com>
2020-05-23 11:23:48 -0500

    CAMBIOS EN TEORÍA 2

--- components/Teoria/T_SNP/m_t_snp.js
+++ components/Teoria/T_SNP/m_t_snp.js
@@ -7,7 +7,7 @@ import styles from '../../Style.js';
 import { RowItem } from "../../elementos/RowItem";
 import { Modal_C1_escena2 } from "../../elementos/ModalsTutorial";
 import { SNC, SNP, SNA, Pruval } from "../../elementos/Modals_teoria";
-
+const { width, height } = Dimensions.get('window')
 const renderPagination = (index, total, context) => {
   return (
     <View style={styles.paginationStyle}>
@@ -106,11 +106,27 @@ export default class m_t_snp  extends React.Component {
       <ScrollView>
       <StatusBar barStyle="dark-content" />
      <View style={style.container}>
-     <Text style={style.title}> DEFINICIÓN</Text>
+     <Text style={style.title}> Definición</Text>
      <Text style={style.text}> El Sistema nervioso periférico conduce información de los sentidos al sistema nervioso central, y lleva información motora fuera de éste.  </Text>
-    <Text style={style.title}> COMPOSICIÓN</Text>
-    <Text style={style.text}>El SNP está compuesto básicamente por: {"\n"}    -Los nervios que nacen del encéfalo y de la médula espinal.{"\n"}    -Los nervios espinales transportan impulsos desde y hacia la médula espinal. {"\n"}    -Los pares craneales transportan impulsos desde y hacia elencéfalo.{"\n"}{"\n"} Estos nervios son cables de comunicación que unen todas las partes del organismo transportando los impulsos desde los receptores sensitivos hasta el CNS y desde el CNS hasta las glándulas o los músculos efectores. </Text>
-    
+    <Text style={style.title}> Composición</Text>
+    <Text style={style.text}>El SNP está compuesto básicamente por: {"\n"}    -Los nervios que nacen del encéfalo y de la médula espinal.{"\n"}    -Los nervios espinales transportan impulsos desde y hacia la médula espinal. {"\n"}    -Los pares craneales transportan impulsos desde y hacia elencéfalo.{"\n"}{"\n"} Estos nervios son cables de comunicación que unen todas las partes del organismo transportando los impulsos desde los receptores sensitivos hasta el CNS y desde el CNS hasta las glándulas o los músculos efectores. {"\n"}{"\n"}{"\n"}</Text>
+
+     </View>
+      </ScrollView>
+       <ScrollView>
+      <StatusBar barStyle="dark-content" />
+     <View style={style.container}>
+     <Text style={style.title}> Clasificación Funcional</Text>
+     <Text style={style.text}> La clasiﬁcación funcional se ocupa tan sólo de las estructuras del SNP. Se clasiﬁca en dos subdivisiones principales  </Text>
+    <Text style={style.title}> División Sensitiva o Aferente</Text>
+    <Text style={style.text}>Consiste en nervios (compuestos por ﬁbras nerviosas) que transportan impulsos hacia el sistema nervioso central desde los receptores sensitivos situados en distintas partes del organismo.{"\n"}Las ﬁbras sensitivas que transportan los impulsos de la piel, los músculos esqueléticos y las articulaciones se denominan ﬁbras sensitivas (aferentes)  somáticas (de  soma, cuerpo). </Text>
+     <Text style={style.title}> División Motora o Eferente</Text>
+    <Text style={style.text}>Transporta los impulsos desde el SNC hasta los órganos efectores: músculos y glándulas. Estos impulsos activan los músculos y las glándulas, es decir, efectúan (provocan) una respuesta motora. La división motora se subdivide a su vez en dos partes.</Text>
+            <Text style={style.title}> Organización del Sistema Nervioso </Text>
+    <View style={style.ContainerImagen}>
+     <Image style={style.Imagen} source={require("../../../assets/images/snp.png")} resizeMode="contain"/>
+       <Text style={style.text}>Organigrama que muestra cómo el SNC  recibe información mediante las fibras sensitivas y da órdenes mediante las fibras motoras. La fibras sensitivas y motoras, juntas, forman los nervios  que constituyen el SNP {"\n"} {"\n"} </Text>
+      </View>
      </View>
       </ScrollView>
 
@@ -190,6 +206,18 @@ const style = StyleSheet.create({
     height :'100%',
     backgroundColor: 'white',
   },
+  Imagen:{
+    
+     height: Dimensions.get('window').height/2,
+     backgroundColor: 'blue',
+  },
+   ContainerImagen:{
+     alignItems: 'center',
+    justifyContent: 'center',
+    
+   // marginBottom: 5
+  },
+
   scrollView: {
     backgroundColor: 'pink',
     marginHorizontal: 8,
@@ -198,15 +226,20 @@ const style = StyleSheet.create({
   title:{ 
 
     textAlign : 'center',
-    fontSize: 25,
-    fontWeight: 'bold'
+    fontSize: 19,
+    fontWeight: 'bold',
+    backgroundColor: '#77c6c6',
+    borderRadius: 50,
+    padding: 3
+
 
   },
   text: {
     textAlign : 'left',
-    fontSize: 20,
+    fontSize: 19,
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
+    margin: 10
   },
 
   button: {
@@ -227,5 +260,12 @@ const style = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height :'100%',
-  }
+  },
+   footer:{
+      flex:0.3,
+      flexDirection: 'row',
+      height :'100%',
+      width : '100%',
+      margin: 15
+     },
 });
